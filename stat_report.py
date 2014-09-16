6d8a251a9a6078e50dd3a2b23abada3d66ec99cf
Pavel Odintsov <pavel.odintsov@gmail.com>
2014-09-16 14:23:39 +0400

    count only ru zone

--- stat_report.py
+++ stat_report.py
@@ -6,7 +6,7 @@ import oursql
 conn = oursql.connect(host='127.0.0.1', user=stat_config.db_user, passwd=stat_config.db_password, db=stat_config.db_name, charset=None, use_unicode=False)
 curs = conn.cursor(oursql.DictCursor)
 
-query = "select asn_normal, count(*) as domain_count, count(*)/(select  count(*) from domains where delegated = 1)*100 as cc from domains where asn != '' group by asn_normal order by cc desc limit 50;"
+query = "select asn_normal, count(*) as domain_count, count(*)/(select count(*) from domains where tld = 'ru' and delegated = 1)*100 as cc from domains where tld = 'ru' and asn != '' group by asn_normal order by cc desc limit 50;"
 
 curs.execute(query)
 
