ec1ddc27bc717031e9f83fc52d309e7435f4dd9b
Alexey Y Manikin <alexey@beget.ru>
2015-05-26 19:47:59 +0300

    Manikin

--- config/main.py
+++ config/main.py
@@ -12,7 +12,7 @@ CURRENT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))+'/../'
 MAX_AS_NUMBER = 500000
 
 MYSQL_HOST = '192.168.2.240'
-MYSQL_PORT = '13307'
+MYSQL_PORT = 13307
 MYSQL_USER = 'domain_statistic'
 MYSQL_PASSWD = 'domain_statisticdomain_statistic'
 MYSQL_DATABASE = 'domain_statistic'
