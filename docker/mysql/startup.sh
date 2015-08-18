f60d7313faf4a8cf6c3a3aebddd510d1acb1e65a
Alexey Y Manikin <alexey@beget.ru>
2015-08-18 16:55:03 +0300

    Testing mysql docker

--- docker/mysql/startup.sh
+++ docker/mysql/startup.sh
@@ -4,12 +4,6 @@ set -e
 mv /dev/log /dev/log.orig       || true
 ln -sf /syslog/log /dev/log     || true
 
-mv /etc/passwd /etc/passwd.orig || true
-ln -sf /nss/passwd /etc/passwd  || true
-
-mv /etc/group /etc/group.orig   || true
-ln -sf /nss/group /etc/group    || true
-
 chmod 644 /etc/resolv.conf /etc/hosts* /etc/localtime || true
 
 ( cd /dev && chmod 666 full null random tty urandom zero ) || true
@@ -21,16 +15,23 @@ mount -o remount,hidepid=2 /proc || true
 mount -o remount,size=50% /dev/shm || true
 
 if ! find /home/mysql/ -maxdepth 0 -empty | xargs -r false; then
+    exec mysqld --verbose --external-locking --delay-key-write=0 --query-cache-size=0
+fi
+
+if  find /home/mysql/ -maxdepth 0 -empty | xargs -r false; then
+    echo "Create base"
+    mkdir /home/mysql || true
+    chmod 777 /home/mysql  || true
     mysql_install_db --basedir=/usr || true
+    mysqld --verbose --external-locking --delay-key-write=0 --query-cache-size=0 &
+    sleep 10
     echo "create database domain_statistic;" | mysql mysql;
-    echo "GRANT ALL PRIVILEGES ON domain_statistic.* TO domain_statistic@localhost IDENTIFIED BY 'domain_statisticdomain_statistic';" | mysql mysql;
+    echo "GRANT ALL PRIVILEGES ON domain_statistic.* TO domain_statistic@% IDENTIFIED BY 'domain_statisticdomain_statistic';" | mysql mysql;
     echo "FLUSH PRIVILEGES;" | mysql mysql;
-    cat /root/stricture.sql | mysql domain_statistic;
+    cat /root/structure.sql | mysql domain_statistic;
     MYPASSWD=$RANDOM$RANDOM$RANDOM
     mysqladmin -u root password $MYPASSWD;
     echo "[client]" > /root/.my.cnf;
-    echo "password = $MYPASSWD" >> /root/.my.cnf;
+    echo "password=$MYPASSWD" >> /root/.my.cnf;
 fi
-
-exec mysqld --verbose --external-locking --delay-key-write=0 --query-cache-size=0
 # EOF
