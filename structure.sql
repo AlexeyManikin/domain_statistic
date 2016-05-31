20b147b951ed7bcb0a51fd3108a49d5b301e68c6
Alexey Manikin <alexey@beget.ru>
2016-05-31 04:01:17 +0300

    Change structure.sql

--- structure.sql
+++ structure.sql
@@ -335,3 +335,18 @@ CREATE TABLE `cname_count_statistic` (
   KEY `date` (`date`),
   KEY `date_tld` (`date`,`tld`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `as_domain_old_count_statistic`;
+CREATE TABLE `as_domain_old_count_statistic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date NOT NULL,
+  `asn` int(11) DEFAULT NULL,
+  `tld` VARCHAR(32) DEFAULT NULL,
+  `old` FLOAT(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `date` (`date`),
+  KEY `asn` (`asn`),
+  KEY `asn_tld` (`asn`, `tld`),
+  KEY `date_tld` (`date`, `tld`),
+  UNIQUE KEY `uniq` (`date`, `tld`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
