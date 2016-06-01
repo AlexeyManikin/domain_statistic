36b288c0a0d29231cc0acba7922ef70d59156848
/usr/bin/vim <alexey@beget.ru>
2016-06-01 12:35:50 +0300

    Manikin

--- docker/runner/install_code.sh
+++ docker/runner/install_code.sh
@@ -4,5 +4,6 @@ mkdir -p /home/domain_statistic;
 git clone -b master -- https://gitlab.beget.ru/amanikin/domain_statictic.git /home/domain_statistic;
 rm -rf /home/domain_statistic/.git;
 rm -rf /home/domain_statistic/docker;
+chmod 700 /home/domain_statistic/bin/bgpdump;
 mkdir /home/domain_statistic/download;
 echo Done;
