be752f2b3ab609cf87abf33f9f3526e1a9bd05a2
/usr/bin/vim <alexey@beget.ru>
2016-06-21 16:07:44 +0300

    Domain group statistic added

--- classes/statistic.py
+++ classes/statistic.py
@@ -7,6 +7,63 @@ from helpers.helpers import get_mysql_connection
 from config.main import START_YEAR, START_MONTH, START_DAY, PREFIX_LIST, MINIMUM_DOMAIN_COUNT
 import datetime
 import MySQLdb
+import pprint
+import traceback
+import multiprocessing
+from config.main import *
+
+
+class WorkerProvaderArray(multiprocessing.Process):
+
+    def __init__(self, number, key, data, zone, date):
+        """
+
+        :param number:
+        """
+        multiprocessing.Process.__init__(self, name="resolver_%s" % number)
+        self.number = number
+        self.connection = None
+        self.key = key
+        self.data = data
+        self.date = date
+        self.zone = zone
+
+    def _connect_mysql(self):
+        """
+        :return:
+        """
+        self.connection = get_mysql_connection()
+
+    def run(self):
+        """
+        Обрабатываем массив записываем в БД
+        :return:
+        """
+        search_string = '.' + self.key + '.'
+        count = 0
+
+        for row in self.data:
+
+            if 'ns1' in row and row['ns1'] is not None and search_string in row['ns1']:
+                count += 1
+            elif 'ns2' in row and row['ns2'] is not None and search_string in row['ns2']:
+                count += 1
+            elif 'ns3' in row and row['ns3'] is not None and search_string in row['ns3']:
+                count += 1
+            elif 'ns4' in row and row['ns4'] is not None and search_string in row['ns4']:
+                count += 1
+
+        sql_insert_date = " ('%s','%s','%s','%s')" % (self.date, self.key, self.zone, count)
+        sql = 'INSERT INTO ns_domain_group_count_statistic(`date`, `ns_group`, `tld`, `count`) VALUE ' \
+              + sql_insert_date
+
+        print("process %s - sql %s" % (self.number, sql))
+
+        self._connect_mysql()
+        cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(sql)
+        self.connection.commit()
+        self.connection.close()
 
 
 class Statistic(object):
@@ -562,18 +619,21 @@ ORDER BY count(*) desc""" % (zone, date, date, MINIMUM_DOMAIN_COUNT)
             for prefix in PREFIX_LIST:
                 self._update_a_domain_old_count_per_zone(date, today, prefix)
 
-    def _update_ns_domain_group_count_statistic(self, date, today, zone):
+    def _update_ns_domain_group_count_statistic_mysql(self, date, today, zone):
         """
+        Отрабатывает очень долго, индексы не проставит, размер таблицы расрастается,
+        функцию ставил на случай чуда в БД
         :type date: date
         :type today: date
         :type zone: unicode
         :return:
         """
         cursor = self.connection.cursor(MySQLdb.cursors.DictCursor)
-        if date <= today:
+        while date <= today:
             ns_array = {}
             provider_array = {}
 
+            # Вариант когда записей нет в БД
             for i in range(1, 5):
                 sql = """SELECT ns%s as ns, count(*) as count FROM domain_history
                         WHERE delegated = 'Y' AND tld = '%s' AND date_start <= '%s' AND date_end >= '%s'
@@ -624,7 +684,6 @@ ORDER BY count(*) desc""" % (zone, date, date, MINIMUM_DOMAIN_COUNT)
                 else:
                     sql_insert += sql_insert_date
 
-
             sql = 'INSERT INTO ns_domain_group_count_statistic(`date`, `ns_group`, `tld`, `count`) VALUE ' \
                   + sql_insert
 
@@ -632,6 +691,118 @@ ORDER BY count(*) desc""" % (zone, date, date, MINIMUM_DOMAIN_COUNT)
             self.connection.commit()
 
             date += datetime.timedelta(days=1)
+
+    def _update_ns_domain_group_count_statistic_python(self, date, today, zone, count=COUNT_THREAD):
+        """
+        Реализация на питоне с использованием процессов
+        :type date: date
+        :type today: date
+        :type zone: unicode
+        :return:
+        """
+        self.connection.close()
+
+        while date <= today:
+            connection = get_mysql_connection()
+            cursor = connection.cursor(MySQLdb.cursors.DictCursor)
+            print("date is %s" % date)
+
+            ns_array = {}
+            provider_array = {}
+
+            sql_has_data = """SELECT count(*) as count FROM ns_count_statistic
+                              WHERE tld = '%s' AND date = '%s'""" % (zone, date)
+            cursor.execute(sql_has_data)
+            data = cursor.fetchone()
+
+            if data['count'] == 0:
+                # Вариант когда записей нет в статистикие по NS серверам
+                for i in range(1, 5):
+                    sql = """SELECT ns%s as ns, count(*) as count FROM domain_history
+                            WHERE delegated = 'Y' AND tld = '%s' AND date_start <= '%s' AND date_end >= '%s'
+                            GROUP BY ns%s
+                            HAVING count(*) > %s
+                            ORDER BY count(*) desc""" % (i, zone, date, date, i, MINIMUM_DOMAIN_COUNT)
+
+                    cursor.execute(sql)
+                    data = cursor.fetchall()
+
+                    for row in data:
+                        if row['ns'] in ns_array:
+                            ns_array[row['ns']] += row['count']
+                        else:
+                            ns_array[row['ns']] = row['count']
+            else:
+                # вариант с использованием уже подготовленных данных
+                sql = """SELECT ns, count FROM ns_count_statistic
+                        WHERE tld = '%s' AND date= '%s'""" % (zone, date)
+                cursor.execute(sql)
+                data = cursor.fetchall()
+                for row in data:
+                    ns_array[row['ns']] = row['count']
+
+            for key in ns_array:
+                try:
+                    if key is not None:
+                        split_ns = key.split('.')
+                        if len(split_ns) > 2:
+
+                            if '.net.ru.' in key or '.com.ua.' in key:
+                                provider = split_ns[-4]
+                            else:
+                                provider = split_ns[-3]
+
+                            if provider in provider_array:
+                                provider_array[provider] += ns_array[key]
+                            else:
+                                provider_array[provider] = ns_array[key]
+
+                except Exception as e:
+                    # ну что бывает, не запоминаем это значение
+                    print("Got an exception: %s" % e.message)
+                    print(traceback.format_exc())
+
+            sql = """SELECT ns1, ns2, ns3, ns4 FROM domain_history
+                    WHERE delegated = 'Y' AND tld = '%s' AND date_start <= '%s' AND date_end >= '%s'
+            """ % (zone, date, date)
+            cursor.execute(sql)
+            data = cursor.fetchall()
+
+            connection.close()
+
+            process_list = []
+            i = 0
+
+            for key in provider_array:
+                search_string = key
+                print("%s Search key is %s" % (i, search_string))
+
+                worker = WorkerProvaderArray(i, search_string, data, zone, date)
+                worker.daemon = True
+                process_list.append(worker)
+                worker.start()
+
+                i += 1
+
+                if i != 0 and i % count == 0:
+                    for process in process_list:
+                        try:
+                            # timeout 2 days
+                            process.join(1728000)
+                        except KeyboardInterrupt:
+                            return
+                    process_list = []
+
+            for process in process_list:
+                try:
+                    # timeout 2 days
+                    process.join(1728000)
+                except KeyboardInterrupt:
+                    return
+
+            date += datetime.timedelta(days=1)
+
+        self.connection = get_mysql_connection()
 
     def update_ns_domain_group_count_statistic(self):
         """
@@ -643,7 +814,7 @@ ORDER BY count(*) desc""" % (zone, date, date, MINIMUM_DOMAIN_COUNT)
 
         if date is not None:
             for prefix in PREFIX_LIST:
-                self._update_ns_domain_group_count_statistic(date, today, prefix)
+                self._update_ns_domain_group_count_statistic_python(date, today, prefix, 16)
 
     def update_all_statistic(self):
         """
