9566e1696ff786690346d024eab24fa98571267c
/usr/bin/vim <alexey@beget.ru>
2016-09-12 13:03:25 +0300

    Fix path

--- docker/runner/install_code.sh
+++ docker/runner/install_code.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p /home/domain_statistic;
-git clone -b master -- https://gitlab.beget.ru/amanikin/domain_statictic.git /home/domain_statistic;
+git clone -b master -- https://github.com/AlexeyManikin/domain_statistic.git /home/domain_statistic;
 rm -rf /home/domain_statistic/.git;
 rm -rf /home/domain_statistic/docker;
 chmod 755 -R /home/domain_statistic/bin;
