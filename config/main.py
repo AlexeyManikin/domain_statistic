8b222e1869bd1f864f11a697ffeb9db2739b8c7e
/usr/bin/vim <alexey@beget.ru>
2020-04-06 01:33:21 +0300

    fix

--- config/main.py
+++ config/main.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# Project server.pyportal
-from __future__ import unicode_literals
-
 __author__ = 'alexeyymnaikin'
 
 import os
@@ -11,26 +8,21 @@ CURRENT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__))+'/../'
 MAX_AS_NUMBER = 450000
 MINIMUM_DOMAIN_COUNT = 50
 
-PREFIX_LIST = ['ru', 'su', 'rf']
+PREFIX_LIST_ZONE = {'ru': 0, 'su': 1, 'rf': 2}
+
 DEFAULT_TIMEOUT = 3.0
 
+
 MYSQL_HOST = 'db'
-MYSQL_PORT = 33060
+MYSQL_PORT = 3306
 MYSQL_USER = 'domain_statistic'
-MYSQL_PASSWD = 'domain_statisticdomain_statistic'
+MYSQL_PASSWD = '12345678'
 MYSQL_DATABASE = 'domain_statistic'
 
 PRKI_JSON_URL = 'http://rpki:8080/export.json'
 
-# for remote connection
-# MYSQL_HOST = 'amanikin.ru'
-# MYSQL_PORT = 3311
-# MYSQL_USER = 'addition'
-# MYSQL_PASSWD = '120686'
-# MYSQL_DATABASE = 'domain_statistic'
+COUNT_THREAD = 400
 
-COUNT_THREAD = 300
-
-START_YEAR = 2015
-START_MONTH = 8
-START_DAY = 1
+START_YEAR = 2020
+START_MONTH = 4
+START_DAY = 3
